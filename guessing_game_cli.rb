bb4c2a18f23f45bcf0e0920e6f41e17bde3195ad
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-17 06:27:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,11 @@
 # Code your solution here!
 
+def prompt_user
+  puts "Enter a guess! Pick a number between 1 and 6"
+end
+
 def run_guessing_game
   random_num = rand(6) + 1
-  p "Enter a guess! Pick a number between 1 and 6"
   user_input = gets.chomp
   
   if user_input == random_num.to_s
